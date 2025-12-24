e31a0a6da5a8102f0db0492f42290de816497b70
Anisa Nurjanah <anisanurjanah2705@gmail.com>
2025-12-25 06:45:29 +0700

    test backend

--- employee-management/src/services/api.js
+++ employee-management/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080",
+  // baseURL: "http://localhost:8080",
+  baseURL: "https://my-json-server.typicode.com/anisanurjanah/juke",
 });
 
 export const getEmployees = () => API.get("/employees");
