4290eae3f121764f0337a0daeeba020e97806445
Anisa Nurjanah <anisanurjanah2705@gmail.com>
2025-12-24 19:04:50 +0700

    test path

--- employee-management/api/server.js
+++ employee-management/api/server.js
@@ -1,13 +1,19 @@
-const jsonServer = require("json-server");
-const path = require("path");
+import jsonServer from "json-server";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const server = jsonServer.create();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const router = jsonServer.router(
-  path.join(process.cwd(), "src", "utils", "employee.json")
+  path.join(__dirname, "..", "..", "src", "utils", "employee.json")
 );
+
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use("/api", router);
 
-module.exports = server;
+export default server;
