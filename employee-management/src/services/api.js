1780aca8c149b7d9ad5dd95950e1af356a515191
Anisa Nurjanah <anisanurjanah2705@gmail.com>
2025-12-25 08:26:36 +0700

    offline handle

--- employee-management/src/services/api.js
+++ employee-management/src/services/api.js
@@ -1,11 +1,24 @@
 import axios from "axios";
 
+const BACKEND_URL = "https://juke-backend.onrender.com";
+const TYPICODE_URL = "https://my-json-server.typicode.com/anisanurjanah/juke";
+
 const API = axios.create({
-  // baseURL: "http://localhost:8080",
-  baseURL: "https://juke-backend.onrender.com",
+  baseURL: BACKEND_URL
 });
 
-export const getEmployees = () => API.get("/employees");
+export const getEmployees = async () => {
+  try {
+    const response = await API.get("/employees");
+    return response.data;
+  } catch (error) {
+    console.warn("Render offline, fallback to Typicode");
+    const typicodeResponse = await axios.get(`${TYPICODE_URL}/employees`);
+    return typicodeResponse.data;
+  }
+};
+
+// export const getEmployees = () => API.get("/employees");
 export const getEmployee = (id) => API.get(`/employees/${id}`);
 export const createEmployee = (data) => API.post("/employees", data);
 export const updateEmployee = (id, data) => API.put(`/employees/${id}`, data);
